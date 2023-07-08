b2332c65e866b74888bea18af6604a210a0967c9
ytb <ytb664@gmail.com>
2023-07-08 11:04:28 +0700

    Adding Materials until Methods Recap (47) in Section 5: Expressions, Statements, & more.

--- IfKeywordAndCodeBlocks/src/MainChallenge.java
+++ IfKeywordAndCodeBlocks/src/MainChallenge.java
@@ -7,30 +7,26 @@ public class MainChallenge {
         int levelCompleted = 5;
         int bonus = 100;
 
-        int finalScore = score;
-
-        calculateScore(true, 800, levelCompleted, bonus);
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("The highscore is " + highScore);
 
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
 
-        finalScore = score;
-
-        if (gameOver) {
-            finalScore += (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-        }
+        System.out.println("The next highscore is " +
+            calculateScore(gameOver, score, levelCompleted, bonus));
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         int finalScore = score;
 
         if (gameOver) {
             finalScore += (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
         }
+
+        return finalScore;
     }
 }
