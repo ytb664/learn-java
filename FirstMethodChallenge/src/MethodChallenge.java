d2f8f7eaa2a354ef588110fdcf9926af69e8e568
ytb <ytb664@gmail.com>
2023-07-08 16:21:00 +0700

    Doing FirstMethodChallenge, which making 2 methods, the first one to print the playerName and position in the highscore ranking, the second one is to calculate the score and divide them to the ranks

---
+++ FirstMethodChallenge/src/MethodChallenge.java
@@ -0,0 +1,46 @@
+public class MethodChallenge {
+
+    public static void main(String[] args) {
+
+        String player = "first";
+        int score = calculateHighScorePosition(1500);
+        displayHighScorePosition(player, score);
+
+        player = "second";
+        score = calculateHighScorePosition(1000);
+        displayHighScorePosition(player, score);
+
+        player = "third";
+        score = calculateHighScorePosition(500);
+        displayHighScorePosition(player, score);
+
+        player = "fourth";
+        score = calculateHighScorePosition(100);
+        displayHighScorePosition(player, score);
+
+        player = "fifth";
+        score = calculateHighScorePosition(25);
+        displayHighScorePosition(player, score);
+
+    }
+
+    public static void displayHighScorePosition(String player, int position) {
+        System.out.println(player + " managed to get into position "
+                + position + " on the high score list");
+    }
+
+    public static int calculateHighScorePosition(int score) {
+
+        int position = 4;
+        if(score >= 1000) {
+            position = 1;
+        } else if (score >= 500) {
+            position =  2;
+        } else if (score >= 100) {
+            position = 3;
+        }
+
+        return position;
+
+    }
+}
