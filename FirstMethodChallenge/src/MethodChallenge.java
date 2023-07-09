f5364fce09620a8ba2181a7768520851fac1b1f1
ytb <ytb664@gmail.com>
2023-07-10 05:29:30 +0700

    Learned about code comparing, edit history, and lastly finishing the first Coding Exercise.

--- FirstMethodChallenge/src/MethodChallenge.java
+++ FirstMethodChallenge/src/MethodChallenge.java
@@ -31,13 +31,15 @@ public class MethodChallenge {
 
     public static int calculateHighScorePosition(int score) {
 
-        int position = 4;
+        int position = 0;
         if(score >= 1000) {
             position = 1;
         } else if (score >= 500) {
             position =  2;
         } else if (score >= 100) {
             position = 3;
+        } else {
+            position = 4;
         }
 
         return position;
