3e30b6fbd49afbb859d92453bc6c82a1065ab993
ytb <ytb664@gmail.com>
2023-07-10 18:06:20 +0700

    Finished Coding Exercise 5: Leap Year Calculator

---
+++ SampleCodingExercise/src/LeapYear.java
@@ -0,0 +1,31 @@
+public class LeapYear {
+
+    public static void main(String[] args) {
+
+        System.out.println(isLeapYear(-1600));
+        System.out.println(isLeapYear(1600));
+        System.out.println(isLeapYear(2017));
+        System.out.println(isLeapYear(2000));
+
+    }
+
+    public static boolean isLeapYear(int year) {
+
+        if (year < 1 || year > 9999) {
+            return false;
+        }
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        }
+        return false;
+
+    }
+
+}
