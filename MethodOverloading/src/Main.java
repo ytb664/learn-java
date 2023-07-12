5b4631b5b61815fe6be43b1d8a93063b33c6b634
ytb <ytb664@gmail.com>
2023-07-12 08:58:52 +0700

    Method Overloading

---
+++ MethodOverloading/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("New score is " + calculateScore("Zim", 500));
+        System.out.println("New score is " + calculateScore(10));
+
+    }
+
+    public static int calculateScore(String playerName, int score) {
+
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+
+    }
+
+    public static int calculateScore(int score) {
+
+        return calculateScore("Anonymous", score);
+
+    }
+
+    public static int calculateScore() {
+
+        System.out.println("no player name, no player score.");
+        return 0;
+
+    }
+
+}
