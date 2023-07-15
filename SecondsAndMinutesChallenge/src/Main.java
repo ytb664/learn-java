0b75dabf9a8513d236572f9000c3287b9f3c2a9a
ytb <ytb664@gmail.com>
2023-07-15 13:53:37 +0700

    Finished Bonus Challenge Solution

--- SecondsAndMinutesChallenge/src/Main.java
+++ SecondsAndMinutesChallenge/src/Main.java
@@ -2,33 +2,38 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(-3945));
+        System.out.println(getDurationString(-65, 45));
+        System.out.println(getDurationString(65, 145));
         System.out.println(getDurationString(65, 45));
+        System.out.println(getDurationString(3945));
     }
 
     public static String getDurationString(int seconds) {
 
-        int minutes = seconds / 60;
-        int hours = minutes / 60;
-        minutes %= 60;
-        seconds %= 60;
-
-        if (seconds >= 0) {
-            return hours + "h " + minutes + "m " + seconds + "s";
+        if (seconds < 0) {
+            return "Invalid data for seconds (" + seconds
+                    + "), must be a positive integer value";
         }
-        return "seconds must be zero or more";
+
+        return getDurationString(seconds / 60, seconds % 60);
     }
 
     public static String getDurationString(int minutes, int seconds) {
 
-        int hours = minutes / 60;
-        minutes %= 60;
-
-        if (minutes >= 0 && (seconds >= 0 && seconds <= 59)) {
-            return hours + "h " + minutes + "m " + seconds + "s";
-        } else if (minutes < 0) {
-            return "minutes must be zero or more";
+        if (minutes < 0) {
+            return "Invalid data for minutes (" + minutes
+                    + "), must be a positive integer value";
         }
-        return "second must be between 0 and 59";
+
+        if (seconds <= 0 || seconds >= 59) {
+            return "Invalid data for seconds (" + seconds
+                    + "), must be between 0 and 59";
+        }
+        int hours = minutes / 60;
+
+        int restOfMin = minutes % 60;
+
+        return hours + "h " + restOfMin + "m " + seconds + "s ";
     }
 }
