ffd35df4ddbaea5b0ffa1a610af8697a27ba5d02
ytb <ytb664@gmail.com>
2023-07-17 11:33:04 +0700

    Finished 58 and 59

---
+++ Section-6-Control-Flow/SwitchExpression/src/Main.java
@@ -0,0 +1,63 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        printDayOfWeek(0);
+        printDayOfWeek(1);
+        printDayOfWeek(2);
+        printDayOfWeek(3);
+        printDayOfWeek(4);
+        printDayOfWeek(5);
+        printDayOfWeek(6);
+        printDayOfWeek(7);
+
+        printWeekDay(0);
+        printWeekDay(1);
+        printWeekDay(2);
+        printWeekDay(3);
+        printWeekDay(4);
+        printWeekDay(5);
+        printWeekDay(6);
+        printWeekDay(7);
+    }
+
+    public static void printDayOfWeek(int day) {
+
+        String dayOfTheWeek = switch(day) {
+            case 0 -> "Sunday";
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            default -> "Invalid Day";
+        };
+
+        System.out.println("day " + day + " is " + dayOfTheWeek);
+    }
+
+    public static void printWeekDay(int day) {
+
+        String dayOfTheWeek;
+        if (day == 0) {
+            dayOfTheWeek = "Sunday";
+        } else if (day == 1) {
+            dayOfTheWeek = "Monday";
+        } else if (day == 2) {
+            dayOfTheWeek = "Tuesday";
+        } else if (day == 3) {
+            dayOfTheWeek = "Wednesday";
+        } else if (day == 4) {
+            dayOfTheWeek = "Thursday";
+        } else if (day == 5) {
+            dayOfTheWeek = "Friday";
+        } else if (day == 6) {
+            dayOfTheWeek = "Saturday";
+        } else {
+            dayOfTheWeek = "Invalid Day";
+        }
+
+        System.out.println("day " + day + " is " + dayOfTheWeek);
+    }
+}
