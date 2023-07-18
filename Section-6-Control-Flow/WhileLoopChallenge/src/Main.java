e42631bac5151ac466a257b7fdb79fafa9cb9fba
ytb <ytb664@gmail.com>
2023-07-18 16:45:01 +0700

    Finished 65. The while loop challenge exercise

---
+++ Section-6-Control-Flow/WhileLoopChallenge/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int number = 4;
+        int endNumber = 20;
+        int evenCount = 0;
+        int oddCount = 0;
+
+        while (number <= endNumber) {
+            number++;
+
+            if (!isEvenNumber(number)) {
+                oddCount++;
+                continue;
+            }
+            evenCount++;
+            System.out.println("Even number " + number);
+            if (evenCount >= 5) {
+                break;
+            }
+        }
+
+        System.out.println("Total odd numbers found = " + oddCount);
+        System.out.println("Total even numbers found = " + evenCount);
+    }
+
+    public static boolean isEvenNumber(int number) {
+
+        return number % 2 == 0;
+    }
+}
