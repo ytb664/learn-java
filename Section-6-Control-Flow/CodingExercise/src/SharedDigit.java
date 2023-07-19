a711fc9bf9a14efe5bb0dd37efc7913d8aae411a
ytb <ytb664@gmail.com>
2023-07-19 17:15:33 +0700

    Finished Coding Exercise 19: Shared Digit

---
+++ Section-6-Control-Flow/CodingExercise/src/SharedDigit.java
@@ -0,0 +1,33 @@
+public class SharedDigit {
+
+    public static void main(String[] args) {
+
+        System.out.println(hasSharedDigit(12,23));
+        System.out.println(hasSharedDigit(9,99));
+        System.out.println(hasSharedDigit(15,55));
+        System.out.println(hasSharedDigit(23,12));
+        System.out.println(hasSharedDigit(12,15));
+        System.out.println(hasSharedDigit(12,43));
+
+    }
+
+    public static boolean hasSharedDigit(int x, int y) {
+
+        if (x < 10 || x > 99 || y < 10 || y > 99) {
+            return false;
+        }
+
+        int x1 = 0;
+        int y1 = 0;
+
+        while (x / 10 != 0) {
+            x1 = x % 10;
+            y1 = y % 10;
+
+            x /= 10;
+            y /= 10;
+        }
+
+        return x == y || x == y1 || x1 == y || x1 == y1;
+    }
+}
