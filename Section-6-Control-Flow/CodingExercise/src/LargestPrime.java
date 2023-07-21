13aec5fb28ba9f010d1af5b9d3e13b7494e30169
ytb <ytb664@gmail.com>
2023-07-21 22:19:28 +0700

    Finished Coding Exercise 26: Largest Prime

---
+++ Section-6-Control-Flow/CodingExercise/src/LargestPrime.java
@@ -0,0 +1,28 @@
+public class LargestPrime {
+
+    public static void main(String[] args) {
+
+        System.out.println(getLargestPrime(16));
+        System.out.println(getLargestPrime(21));
+        System.out.println(getLargestPrime(217));
+        System.out.println(getLargestPrime(0));
+        System.out.println(getLargestPrime(45));
+        System.out.println(getLargestPrime(-1));
+    }
+
+    public static int getLargestPrime(int number) {
+
+        if (number <= 1) {
+            return -1;
+        }
+
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                number /= i;
+                i--;
+            }
+        }
+
+        return number;
+    }
+}
