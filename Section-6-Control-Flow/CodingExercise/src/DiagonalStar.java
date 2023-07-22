42b4e1a5d41cba9875121a36ce3037764dc04818
ytb <ytb664@gmail.com>
2023-07-22 13:41:51 +0700

    Finished Coding Exercise 27: Diagonal Star

---
+++ Section-6-Control-Flow/CodingExercise/src/DiagonalStar.java
@@ -0,0 +1,35 @@
+public class DiagonalStar {
+
+    public static void main(String[] args) {
+
+        printSquareStar(5);
+        printSquareStar(8);
+    }
+
+    public static void printSquareStar(int number) {
+
+        if (number < 5) {
+            System.out.println("Invalid Value");
+        }
+
+        for (int i = 1; i <= number; i++) {
+
+            for (int j = 1; j <= number; j++) {
+
+                if (i == 1 || i == number || j == 1 || j == number) {
+                    System.out.print("*");
+                } else if (i == j) {
+                    System.out.print("*");
+                } else if (j == (number - i + 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+    }
+}
