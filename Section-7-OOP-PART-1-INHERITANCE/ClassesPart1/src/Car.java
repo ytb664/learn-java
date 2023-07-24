aa3d04c00718709ab5b228619dbec5bfddfa72dc
ytb <ytb664@gmail.com>
2023-07-24 22:45:00 +0700

    Finished 77. Introduction to Classes, Using Getters Method

--- Section-7-OOP-PART-1-INHERITANCE/ClassesPart1/src/Car.java
+++ Section-7-OOP-PART-1-INHERITANCE/ClassesPart1/src/Car.java
@@ -1,10 +1,30 @@
 public class Car {
 
-    private String make;
-    private String model;
-    private String color;
-    private int doors;
-    private boolean convertible;
+    private String make = "Tesla";
+    private String model = "Model X";
+    private String color = "Gray";
+    private int doors = 2;
+    private boolean convertible = true;
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
 
     public void describeCar() {
 
@@ -13,5 +33,5 @@ public class Car {
                 make + " " +
                 model + " " +
                 (convertible ? "Convertible" : ""));
-    } 
+    }
 }
--- Section-7-OOP-PART-1-INHERITANCE/ClassesPart1/src/Main.java
+++ Section-7-OOP-PART-1-INHERITANCE/ClassesPart1/src/Main.java
@@ -2,5 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Car car = new Car();
+//        car.make = "Porsche";
+//        car.model = "Carrera";
+//        car.color = "Red";
+        System.out.println("make = " + car.getMake());
+        System.out.println("model = " + car.getModel());
+        car.describeCar();
     }
 }
