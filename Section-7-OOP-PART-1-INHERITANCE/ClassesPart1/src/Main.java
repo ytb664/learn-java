ac943f54f1e7c801ca6561a176f1cd3e306345fb
ytb <ytb664@gmail.com>
2023-07-26 12:22:32 +0700

    Finished 78

--- Section-7-OOP-PART-1-INHERITANCE/ClassesPart1/src/Main.java
+++ Section-7-OOP-PART-1-INHERITANCE/ClassesPart1/src/Main.java
@@ -3,11 +3,22 @@ public class Main {
     public static void main(String[] args) {
 
         Car car = new Car();
-//        car.make = "Porsche";
-//        car.model = "Carrera";
-//        car.color = "Red";
+        car.setMake("Porsche");
+        car.setModel("Carrera");
+        car.setDoors(2);
+        car.setConvertible(true);
+        car.setColor("black");
         System.out.println("make = " + car.getMake());
         System.out.println("model = " + car.getModel());
         car.describeCar();
+
+        Car targa = new Car();
+        targa.setMake("Porsche");
+        targa.setModel("Targa");
+        targa.setDoors(2);
+        targa.setConvertible(false);
+        targa.setColor("red");
+
+        targa.describeCar();
     }
 }
