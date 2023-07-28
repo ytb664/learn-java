14e1fe40c801d68b25cdfaafe66a980c5cc2b78d
ytb <ytb664@gmail.com>
2023-07-28 21:34:26 +0700

    Finished 87

--- Section-7-OOP-PART-1-INHERITANCE/RecordProject/src/Main.java
+++ Section-7-OOP-PART-1-INHERITANCE/RecordProject/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 5; i++) {
-            Student s = new Student("S92300" + i,
+            LPAStudent s = new LPAStudent("S92300" + i,
                     switch(i) {
                 case 1 -> "Mary";
                 case 2 -> "Carol";
@@ -16,5 +16,21 @@ public class Main {
                     "Java Masterclass");
             System.out.println(s);
         }
+
+        Student pojoStudent = new Student("5923006", "Ann",
+                "05/11/1985", "Java Masterclass");
+        LPAStudent recordStudent = new LPAStudent("5923007", "Bill",
+                "05/11/1985", "Java Masterclass");
+
+        System.out.println(pojoStudent);
+        System.out.println(recordStudent);
+
+        pojoStudent.setClassList(pojoStudent.getClassList() + ", Java OCP Exam 829");
+//        recordStudent.setClassList(recordStudent.classList() + ", Java OCP Exam 829");
+
+        System.out.println(pojoStudent.getName() + " is taking " +
+                pojoStudent.getClassList());
+        System.out.println(recordStudent.name() + " is taking " +
+                recordStudent.classList());
     }
 }
