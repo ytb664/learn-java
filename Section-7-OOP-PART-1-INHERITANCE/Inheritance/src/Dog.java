047ec102875921ff9ecfd03468f1d16f61a8dcdd
ytb <ytb664@gmail.com>
2023-07-30 22:03:32 +0700

    Finished 91

--- Section-7-OOP-PART-1-INHERITANCE/Inheritance/src/Dog.java
+++ Section-7-OOP-PART-1-INHERITANCE/Inheritance/src/Dog.java
@@ -28,11 +28,41 @@ public class Dog extends Animal {
 
     public void makeNoise() {
 
+        if(type == "Wolf") {
+            System.out.print("Ow Woooo! ");
+        }
+        bark();
+        System.out.println();
     }
 
     @Override
     public void move(String speed) {
         super.move(speed);
-        System.out.println("Dogs walk, run and wag their tail");
+//        System.out.println("Dogs walk, run and wag their tail");
+        if (speed == "slow") {
+            walk();
+            wagTail();
+        } else {
+            run();
+            bark();
+        }
+
+        System.out.println();
+    }
+
+    private void bark() {
+        System.out.print("Woof! ");
+    }
+
+    private void run() {
+        System.out.print("Dog Running ");
+    }
+
+    private void walk() {
+        System.out.print("Dog Walking ");
+    }
+
+    private void wagTail() {
+        System.out.print("Tail Wagging ");
     }
 }
