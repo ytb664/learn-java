e5df6357f9ff67ae848a1e611d2ff79aefda523c
ytb <ytb664@gmail.com>
2023-08-05 11:08:08 +0700

    Finished 104

---
+++ Section-8-OOP-PART-2-POLYMORPHISM/Encapsulation/Encapsulation/src/Player.java
@@ -0,0 +1,26 @@
+public class Player {
+
+    public String fullName;
+    public int health;
+    public String weapon;
+
+    public void loseHealth(int damage) {
+
+        health -= damage;
+        if (health <= 0) {
+            System.out.println("Player knocked out of game");
+        }
+    }
+
+    public void restoreHealth(int extraHealth) {
+        health += extraHealth;
+    }
+
+    public int healthRemaining() {
+        if (health > 100) {
+            System.out.println("Player restored to 100%");
+            health = 100;
+        }
+        return health;
+    }
+}
