1472bdd8f8afd0669294393522e3ec9c1932dbea
ytb <ytb664@gmail.com>
2023-08-06 16:06:18 +0700

    Finished 106, 107, & 108

---
+++ Section-8-OOP-PART-2-POLYMORPHISM/Encapsulation/Polymorphism/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        Movie theMovie = Movie.getMovie("Science", "Star Wars");
+//        theMovie.watchMovie();
+
+        Scanner s = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter type (A for Adventure, C for Comedy, " +
+                    "S for Science Fiction, or Q to quit): ");
+            String type = s.nextLine();
+            if ("Qq".contains(type)) {
+                break;
+            }
+            System.out.print("Enter Movie Title: ");
+            String title = s.nextLine();
+            Movie movie = Movie.getMovie(type, title);
+            movie.watchMovie();
+        }
+
+//        Movie secondMovie = new Comedy("Ant-Man");
+//        secondMovie.watchMovie();
+//
+//        Movie thirdMovie = new ScienceFiction("Guardian of The Galaxy");
+//        thirdMovie.watchMovie();
+    }
+}
