0fbfb7135e0d0d32d9f57b8cd39a9f1c5bed92d2
ytb <ytb664@gmail.com>
2023-08-08 22:56:25 +0700

    Finished 111 and Coding Exercise 40

--- Section-8-OOP-PART-2-POLYMORPHISM/Encapsulation/PolymorphismChallenge/src/Car.java
+++ Section-8-OOP-PART-2-POLYMORPHISM/Encapsulation/PolymorphismChallenge/src/Car.java
@@ -3,140 +3,185 @@ public class Car {
     private String description;
 
     public Car(String description) {
-        description = description.toLowerCase();
-        String rest = description.substring(1);
-        char initial = description.toUpperCase().charAt(0);
-
-        this.description = initial + rest;
+        this.description = description;
     }
 
     public void startEngine() {
+        System.out.println("Car -> startEngine");
+    }
 
-        String instanceType = this.getClass().getSimpleName();
-        System.out.println(description + ", " +
-                instanceType + " car is starting the engine");
+    protected void runEngine() {
+        System.out.println("Car -> runEngine");
     }
 
     public void drive() {
-
-        String instanceType = this.getClass().getSimpleName();
-        System.out.println("Driving " + instanceType);
+        System.out.println("Car -> driving, type is " +
+                getClass().getSimpleName());
+        runEngine();
     }
 
-    protected void runEngine(int distance) {
-
-        System.out.println("Engine is running, to achieve destination we need: ");
-    }
-
-    public static Car getCar(String type, String description) {
-
-        return switch (type.toUpperCase().charAt(0)) {
-            case 'G' -> new GasPoweredCar(description, 6);
-            case 'E' -> new ElectricCar(description, 60);
-            case 'H' -> new HybridCar(description, 60, 6);
-            default ->  new Car(description);
-        };
-    }
+    //    public Car(String description) {
+//        description = description.toLowerCase();
+//        String rest = description.substring(1);
+//        char initial = description.toUpperCase().charAt(0);
+//
+//        this.description = initial + rest;
+//    }
+//
+//    public void startEngine() {
+//
+//        String instanceType = this.getClass().getSimpleName();
+//        System.out.println(description + ", " +
+//                instanceType + " car is starting the engine");
+//    }
+//
+//    public void drive() {
+//
+//        String instanceType = this.getClass().getSimpleName();
+//        System.out.println("Driving " + instanceType);
+//    }
+//
+//    protected void runEngine(int distance) {
+//
+//        System.out.println("Engine is running, to achieve destination we need: ");
+//    }
+//
+//    public static Car getCar(String type, String description) {
+//
+//        return switch (type.toUpperCase().charAt(0)) {
+//            case 'G' -> new GasPoweredCar(description, 6);
+//            case 'E' -> new ElectricCar(description, 60);
+//            case 'H' -> new HybridCar(description, 60, 6);
+//            default ->  new Car(description);
+//        };
+//    }
 }
 
 class GasPoweredCar extends Car {
 
-    private double avgKmPerLitre;
-    private int cylinders;
+    private double avgKmPerLiter;
+    private int cylinders = 6;
 
-    public GasPoweredCar(String description, int cylinders) {
+    public GasPoweredCar(String description) {
         super(description);
-        this.cylinders = cylinders <= 0 ? -1 : cylinders;
-        this.avgKmPerLitre = Math.round((100.0 / cylinders));
+    }
+
+    public GasPoweredCar(String description, double avgKmPerLiter,
+                         int cylinders) {
+        super(description);
+        this.avgKmPerLiter = avgKmPerLiter;
+        this.cylinders = cylinders;
     }
 
     @Override
     public void startEngine() {
-        super.startEngine();
-        System.out.printf("> %s%n".repeat(3),
-                "COOL NOISE",
-                "Attracting Attention",
-                "Engine ON");
-        System.out.println("-".repeat(10));
+        System.out.printf("Gas -> All %d cylinders are fired up, Ready!%n", cylinders);
     }
 
     @Override
-    public void drive() {
-        super.drive();
-        String textBlock = """
-                > With a Stick Shift
-                Using $avgKmPerLitreKM/L of gas""".replace("$avgKmPerLitre",
-                String.valueOf(Math.round(avgKmPerLitre)));
-        System.out.println(textBlock + "\n" + "-".repeat(10));
-    }
-
-    @Override
-    protected void runEngine(int distance) {
-        super.runEngine(distance);
-        System.out.println(Math.round(distance / avgKmPerLitre) + "L of Gas");
+    protected void runEngine() {
+        System.out.printf("Gas -> usage exceeds the average: %.2f %n", avgKmPerLiter);
     }
 }
 
-class ElectricCar extends Car {
-
-    private double avgKmPerCharge;
-    private int batterySize;
-
-    public ElectricCar(String description, int batterySize) {
-        super(description);
-        this.batterySize = batterySize <= 0 ? -1 : batterySize;
-        this.avgKmPerCharge = batterySize * 0.15d;
-    }
-
-    @Override
-    public void startEngine() {
-        super.startEngine();
-        System.out.printf("> %s%n".repeat(3),
-                "No Noise",
-                "Smooth Start",
-                "Infotainment System Said Engine ON");
-        System.out.println("-".repeat(10));
-    }
-
-    @Override
-    public void drive() {
-        super.drive();
-        String textBlock = "> Starting Auto-Pilot";
-        System.out.println(textBlock + "\n" + "-".repeat(10));
-    }
-
-    @Override
-    protected void runEngine(int distance) {
-        super.runEngine(distance);
-        System.out.println(Math.round(distance / avgKmPerCharge) + "x of Charging");
-    }
-}
-
-class HybridCar extends Car {
-
-    private double avgKmPerLitre;
-    private int batterySize;
-    private int cylinders;
-
-    public HybridCar(String description, int batterySize, int cylinders) {
-        super(description);
-        this.batterySize = batterySize <= 0 ? -1 : batterySize;
-        this.cylinders = cylinders <= 0 ? -1 : cylinders;
-        this.avgKmPerLitre = ((batterySize * 0.15d) + (100.0 / cylinders)) / 2;
-    }
-
-    @Override
-    public void startEngine() {
-        super.startEngine();
-        System.out.printf("> %s%n".repeat(2),
-                "Light Noise",
-                "the Gas powered engine is on");
-        System.out.println("-".repeat(10));
-    }
-
-    @Override
-    protected void runEngine(int distance) {
-        super.runEngine(distance);
-        System.out.println(Math.round(distance / avgKmPerLitre) + "L of Gas");
-    }
-}
+//class GasPoweredCar extends Car {
+//
+//    private double avgKmPerLitre;
+//    private int cylinders;
+//
+//    public GasPoweredCar(String description, int cylinders) {
+//        super(description);
+//        this.cylinders = cylinders <= 0 ? -1 : cylinders;
+//        this.avgKmPerLitre = Math.round((100.0 / cylinders));
+//    }
+//
+//    @Override
+//    public void startEngine() {
+//        super.startEngine();
+//        System.out.printf("> %s%n".repeat(3),
+//                "COOL NOISE",
+//                "Attracting Attention",
+//                "Engine ON");
+//        System.out.println("-".repeat(10));
+//    }
+//
+//    @Override
+//    public void drive() {
+//        super.drive();
+//        String textBlock = """
+//                > With a Stick Shift
+//                Using $avgKmPerLitreKM/L of gas""".replace("$avgKmPerLitre",
+//                String.valueOf(Math.round(avgKmPerLitre)));
+//        System.out.println(textBlock + "\n" + "-".repeat(10));
+//    }
+//
+//    @Override
+//    protected void runEngine(int distance) {
+//        super.runEngine(distance);
+//        System.out.println(Math.round(distance / avgKmPerLitre) + "L of Gas");
+//    }
+//}
+//
+//class ElectricCar extends Car {
+//
+//    private double avgKmPerCharge;
+//    private int batterySize;
+//
+//    public ElectricCar(String description, int batterySize) {
+//        super(description);
+//        this.batterySize = batterySize <= 0 ? -1 : batterySize;
+//        this.avgKmPerCharge = batterySize * 0.15d;
+//    }
+//
+//    @Override
+//    public void startEngine() {
+//        super.startEngine();
+//        System.out.printf("> %s%n".repeat(3),
+//                "No Noise",
+//                "Smooth Start",
+//                "Infotainment System Said Engine ON");
+//        System.out.println("-".repeat(10));
+//    }
+//
+//    @Override
+//    public void drive() {
+//        super.drive();
+//        String textBlock = "> Starting Auto-Pilot";
+//        System.out.println(textBlock + "\n" + "-".repeat(10));
+//    }
+//
+//    @Override
+//    protected void runEngine(int distance) {
+//        super.runEngine(distance);
+//        System.out.println(Math.round(distance / avgKmPerCharge) + "x of Charging");
+//    }
+//}
+//
+//class HybridCar extends Car {
+//
+//    private double avgKmPerLitre;
+//    private int batterySize;
+//    private int cylinders;
+//
+//    public HybridCar(String description, int batterySize, int cylinders) {
+//        super(description);
+//        this.batterySize = batterySize <= 0 ? -1 : batterySize;
+//        this.cylinders = cylinders <= 0 ? -1 : cylinders;
+//        this.avgKmPerLitre = ((batterySize * 0.15d) + (100.0 / cylinders)) / 2;
+//    }
+//
+//    @Override
+//    public void startEngine() {
+//        super.startEngine();
+//        System.out.printf("> %s%n".repeat(2),
+//                "Light Noise",
+//                "the Gas powered engine is on");
+//        System.out.println("-".repeat(10));
+//    }
+//
+//    @Override
+//    protected void runEngine(int distance) {
+//        super.runEngine(distance);
+//        System.out.println(Math.round(distance / avgKmPerLitre) + "L of Gas");
+//    }
+//}
--- Section-8-OOP-PART-2-POLYMORPHISM/Encapsulation/PolymorphismChallenge/src/Main.java
+++ Section-8-OOP-PART-2-POLYMORPHISM/Encapsulation/PolymorphismChallenge/src/Main.java
@@ -1,34 +1,44 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 
+        Car car = new Car("2022 Blue Ferrari 296 GTS");
+        runRace(car);
+
+        Car ferrari = new GasPoweredCar("2022 Blue Ferrari 296 GTS",
+                15.4, 6);
+        runRace(ferrari);
 //        Object car = Car.getCar("Gas", "Porsche");
 //        GasPoweredCar porsche = (GasPoweredCar) car;
 //        porsche.startEngine();
 //        porsche.drive();
 
-        Scanner s = new Scanner(System.in);
-        while (true) {
-            System.out.printf("What's your car type? %s %n",
-                    "'G' for Gas Car, 'E' for Electric, 'H' for Hybrid, 'Q' to quit");
-            String type = s.nextLine();
+//        Scanner s = new Scanner(System.in);
+//        while (true) {
+//            System.out.printf("What's your car type? %s %n",
+//                    "'G' for Gas Car, 'E' for Electric, 'H' for Hybrid, 'Q' to quit");
+//            String type = s.nextLine();
+//
+//            if ("Qq".contains(type)) {
+//                break;
+//            }
+//
+//            System.out.print("What's your car? ");
+//            String description = s.nextLine();
+//
+//            System.out.print("What's the distance of your destination? ");
+//            int distance = Integer.parseInt(s.nextLine());
+//
+//            Car car = Car.getCar(type, description);
+//            car.startEngine();
+//            car.drive();
+//            car.runEngine(distance);
+//        }
+    }
 
-            if ("Qq".contains(type)) {
-                break;
-            }
+    public static void runRace(Car car) {
 
-            System.out.print("What's your car? ");
-            String description = s.nextLine();
-
-            System.out.print("What's the distance of your destination? ");
-            int distance = Integer.parseInt(s.nextLine());
-
-            Car car = Car.getCar(type, description);
-            car.startEngine();
-            car.drive();
-            car.runEngine(distance);
-        }
+        car.startEngine();
+        car.drive();
     }
 }
