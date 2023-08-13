19c03a64986c37f3edfcfae92f7f96e301f000d8
ytb <ytb664@gmail.com>
2023-08-13 18:19:36 +0700

    Finished 116, marked the end of Section 8: OOP Part 2 - Polymorphism

---
+++ Section-8-OOP-PART-2-POLYMORPHISM/Encapsulation/Packages/src/dev/lpa/Main.java
@@ -0,0 +1,13 @@
+package dev.lpa;
+
+import com.abc.first.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Item firstItem = new com.abc.first.Item("Burger");
+//        com.abc.first.Item firstItem = new com.abc.first.Item("Burger");
+        System.out.println(firstItem);
+    }
+}
